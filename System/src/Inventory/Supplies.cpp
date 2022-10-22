a2ffa056823f58b5d029129574e0291330d9ac8c
Ettienne Van Zyl <u19012366@tuks.co.za>
2022-10-22 19:13:44 +0200

    Add doxygen comments to Supplies.cpp

--- System/src/Inventory/Supplies.cpp
+++ System/src/Inventory/Supplies.cpp
@@ -2,16 +2,23 @@
 
 using namespace std;
 
+/** 
+*  @file Supplies.h
+*  @brief Supplies class implementation,
+*  Design Patterns:
+*  @li Template Method: Concrete class participant
+*  @li Prototype: Concrete Prototype participant
+*  @li Decorator: Concrete Component participant
+*  @li Factory: Concrete participant
+*
+*  @details Supplies implements the Entity interface, and is it's concrete class
+*  
+*  @author Marthinus Ettienne van Zyl ; u19012366,
+*TODO:  @todo Override base class decorator extra method
+*/
+
 Supplies::Supplies() {
 }
 
 Supplies::~Supplies() {
-}
-
-int Supplies::getMember() {
-  return this->member;
-}
-
-void Supplies::setMember(int member) {
-  this->member = member;
 }
