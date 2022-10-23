342f5fc4332be39f4a2da038855fb78635898e91
Ruan Carlinsky gmovernight <ruancarlinsky01@gmail.com>
2022-10-23 09:10:36 +0200

    Invoker.h Member & Functions updated.

--- System/include/TakeActions/Invoker.h
+++ System/include/TakeActions/Invoker.h
@@ -2,14 +2,23 @@
 #define INVOKER_H
 
 class Invoker {
+  #include <TakeActions/Action.h>
   private:
     int member;
+    Action * actions = new Action[6];
 
   public:
     Invoker();
     ~Invoker();
     int getMember();
     void setMember(int);
+    
+    void defendAlliance();
+    void attackEnemyAlliance();
+    void hold();
+    void retreat();
+    void surrender();
+    void requestAlly();
 };
 
 #endif
