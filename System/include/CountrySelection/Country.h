559398702c5b9c1d6ebf26cc91b766ea1813fb04
Ettienne Van Zyl <u19012366@tuks.co.za>
2022-10-23 22:56:09 +0200

    Implemented Country class

--- System/include/CountrySelection/Country.h
+++ System/include/CountrySelection/Country.h
@@ -1,15 +1,35 @@
 #ifndef COUNTRY_H
 #define COUNTRY_H
 
-class Country {
+#include <string>
+#include <vector>
+
+#include <ObserverPattern/ActionListener.h>
+#include <CountrySelection/Statistics.h>
+#include <Inventory/InventoryShop.h>
+
+class Country : ActionListener{
   private:
-    int member;
+    std::string _name;
+    std::vector<Country>* _allies;
+    Statistics* _stats;
+    InventoryShop* _inventoryShop;
 
   public:
     Country();
+    Country(std::string, std::vector<Country>*, Statistics*, InventoryShop*);
     ~Country();
-    int getMember();
-    void setMember(int);
+    
+    std::string getName() const;
+    std::vector<Country> getAllies() const;
+    Statistics getStats() const;
+    InventoryShop getInventoryShop() const;
+
+    void addAllies(std::vector<Country>);
+    void addAlly(Country);
+
+    void removeAlly(Country);
+    void removeAllies(std::vector<Country>);
 };
 
 #endif
--- System/src/CountrySelection/Country.cpp
+++ System/src/CountrySelection/Country.cpp
@@ -5,13 +5,59 @@ using namespace std;
 Country::Country() {
 }
 
+Country::Country(string name, vector<Country>* allies, Statistics* stats, InventoryShop* inventoryShop) {
+  _name = name;
+  _allies = allies;
+  _stats = stats;
+  _inventoryShop = inventoryShop;
+}
+
 Country::~Country() {
+  delete _allies;
+  delete _stats;
+  delete _inventoryShop;
 }
 
-int Country::getMember() {
-  return this->member;
+string Country::getName() const {
+  return _name;
 }
 
-void Country::setMember(int member) {
-  this->member = member;
+vector<Country> Country::getAllies() const {
+  return *_allies;
+}
+
+Statistics Country::getStats() const {
+  return *_stats;
+}
+
+InventoryShop Country::getInventoryShop() const {
+  return *_inventoryShop;
+}
+
+void Country::addAllies(vector<Country> allies) {
+  for (int i = 0; i < allies.size(); i++) {
+    _allies->push_back(allies[i]);
+  }
+}
+
+void Country::addAlly(Country ally) {
+  _allies->push_back(ally);
+}
+
+void Country::removeAlly(Country ally) {
+  for (int i = 0; i < _allies->size(); i++) {
+    if (_allies->at(i).getName() == ally.getName()) {
+      _allies->erase(_allies->begin() + i);
+    }
+  }
+}
+
+void Country::removeAllies(vector<Country> toRemove) {
+  for (int i = 0; i < toRemove.size(); i++) {
+    for (int j = 0; j < _allies->size(); j++) {
+      if (_allies->at(j).getName() == toRemove[i].getName()) {
+        _allies->erase(_allies->begin() + j);
+      }
+    }
+  }
 }
