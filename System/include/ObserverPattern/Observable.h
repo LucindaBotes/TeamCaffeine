c9ce0cd9bfb685caaad70a764b8037f66fcf78cf
Ruan Carlinsky gmovernight <ruancarlinsky01@gmail.com>
2022-10-24 20:59:19 +0200

    Added Observable Abstract Class

---
+++ System/include/ObserverPattern/Observable.h
@@ -0,0 +1,21 @@
+#ifndef OBSERVABLE_H
+#define OBSERVABLE_H
+
+#include <ObserverPattern/ActionListener.h>
+#include <vector>
+
+class Observable {
+    private:
+        int member;
+        std::vector<ActionListener*> actionListeners;
+
+    public:
+        Observable();
+        ~Observable();
+        int getMember();
+        void setMember(int);
+        virtual void addListener(ActionListener * listener) = 0;
+        virtual void removeListener(ActionListener * listener) = 0;
+        virtual void notifyListeners() = 0;
+};
+#endif
