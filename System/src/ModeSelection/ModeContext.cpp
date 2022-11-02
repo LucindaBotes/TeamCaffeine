99e5d3c958f7baaf501687db705646bffc8ada69
UnarineLeo <unarineleo@gmail.com>
2022-11-02 13:53:41 +0200

    undo

--- System/src/ModeSelection/ModeContext.cpp
+++ System/src/ModeSelection/ModeContext.cpp
@@ -12,11 +12,13 @@ ModeContext::~ModeContext()
     delete _strategy;
 }
 
+/// @brief just notifies the user about the perks of choosing either real-mode or design mode
 void ModeContext::start()
 {
     _strategy->startSimulation();
 }
 
+/// @brief undo the recent action
 void ModeContext::Undo()
 {
     _strategy->undo();
