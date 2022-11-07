b07a5879ac55f61cc195c2bec423d96895dc0418
u19012366 <u19012366@tuks.co.za>
2022-11-07 02:39:49 +0200

    Merged Randomizer classes

--- System/src/HelperClasses/Randomizer.cpp
+++ System/src/HelperClasses/Randomizer.cpp
@@ -11,7 +11,8 @@ using namespace std;
 *
 *  @details Randomizer implements the base @file Country.h class.
 *  
-*  @author Marthinus Ettienne van Zyl ; u19012366
+*  @author Marthinus Ettienne van Zyl ; u19012366,
+*  @author Ruan Tristan Carlinsky ; u20416823
 */
 
 
@@ -67,4 +68,28 @@ double Randomizer::getUniformRandomNumber(double min, double max) {
 int Randomizer::getUniformRandomNumber(int min, int max) {
   std::uniform_int_distribution<int> toReturnDistrib (min, max);
   return toReturnDistrib(_seedGenerator);
+}
+
+/**
+ * @brief method to get a random boolean true or false with uniform distribution(50% chance of true and 50% chance of false) 
+ * 
+ * @return true 
+ * @return false 
+ */
+bool Randomizer::getRandomBoolean()
+{
+    static const int shift = static_cast<int>(std::log2(RAND_MAX));
+    return (rand() >> shift) & 1;
+}
+
+/**
+ * @brief method to get a random integer between the [1 and max values]
+ * 
+ * @param maxValue 
+ * @return int 
+ */
+int Randomizer::getRandomInt(int maxValue)
+{
+    srand(time(0));
+    return (rand() % maxValue) + 1;
 }
